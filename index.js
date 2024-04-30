81bbd513cba66931e6245c86fc6875782955e484
Harsh-1243 <harshpanchal1243@gmail.com>
2024-04-30 23:41:09 +0530

    added code

--- index.js
+++ index.js
@@ -1,28 +1,78 @@
-let btn = document.querySelector("button");
+const search = document.querySelector(".search-btn");
 
-let baseUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=new york&appid=16414a134279fddd6a2d4d8b4bd4afb0`;
+let BaseUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
 
+const ApiKey = "&appid=16414a134279fddd6a2d4d8b4bd4afb0";
 
-btn.addEventListener("click",async()=>{
+const container = document.querySelector(".weather-cont");
 
-    await fetch(baseUrl).then(async(res)=>{
-         
-        console.log("this is response", res);
+const error404 = document.querySelector(".not-found");
 
-        await res.json().then((values)=>{
+search.addEventListener("click",()=>{
 
-            console.log("this is value",values);
+    const CityName = document.querySelector("input").value;
 
-        }).catch((valuesError)=>{
+    if (CityName === "") {
 
-            console.log("this is valueError", valuesError);
+        alert("ENTER CITY NAME");
+        return;
+        
+    }
+    else{
+
+        fetch(BaseUrl + `&q=${CityName}` + ApiKey).then((res)=>{
+
+            res.json().then((json)=>{
+
+                if (json.cod  === "404") {
+                    
+                    container.style.height = "370px";
+                    error404.classList.remove("opacity-0");
+                    error404.classList.remove("d-none");
+                    error404.classList.add("fade-animation");
+
+                } else {
+
+                    error404.classList.add("opacity-0");
+                    error404.classList.add("d-none");
+                    error404.classList.remove("fade-animation");
+
+                    const img = document.querySelector(".weather-box img");
+                    const temp = document.querySelector(".temp span");
+                    const desc = document.querySelector(".desc span");
+                    const humidity = document.querySelectorAll(".info span")[0];
+                    const wind = document.querySelectorAll(".info span")[1];
+                    const weather_box = document.querySelector(".weather-box");
+                    const weather_details = document.querySelector(".weather-details");
+
+                    img.src = `images/${json.weather[0].main}.png`;
+                    
+                    weather_box.classList.remove("opacity-0");
+                    weather_box.classList.add("fade-animation");
+                    weather_details.classList.remove("opacity-0");
+                    weather_details.classList.add("fade-animation");
+                    temp.innerHTML = `${Math.round(json.main.temp)}°C`;
+                    desc.innerHTML = `${json.weather[0].description}`;
+                    humidity.innerHTML = `${json.main.humidity}%`;
+                    wind.innerHTML = `${json.wind.speed}Km/h`;
+                    container.style.height = "550px";
+                    
+                }   
+
+            }).catch((json_err)=>{
+
+                console.log(json_err);
+
+            })
+
+        }).catch((err)=>{
+
+            console.log(err);
 
         })
         
-    }).catch((err)=>{
+    }   
 
-        console.log("this is err",err);
-        
-    })
-})
+});
+
 
